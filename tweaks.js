dbdc22afdbcc52fea59ae501c60a6ac94dfd5419
Oleg Gunkin <oleg.gunkin@gmail.com>
2013-12-05 01:32:23 -0800

    static map functions

--- tweaks.js
+++ tweaks.js
@@ -1,38 +1,92 @@
-$(document).ready(function () {
-	var getChevronHtml = function (direction) {
-		return "<i class='glyphicon glyphicon-chevron-" + direction + "'></i>";
-	};
+(function () {
+	var drawMap = function (){
+		$(".static-gmap").each(function () {
+			var $this = $(this);
+			var addressEncoded = encodeURI($this.text());
+			var mapImgUrl;
 
-	(function mobileMenuRetract() {
-		var $menu   = $("#navbar-collapse");
-		var $toggle = $("#header .navbar-toggle");
+			// $this.text("");
+			$this.css({
+				"display" : "block",
+				"width": "100%"
+			});
 
-		$menu.on("click", "a", function () {
-			if ($toggle.is(":visible") && $menu.is(":visible")) {
-				$menu.collapse("hide");
+			if (!$this.data("map-width")) {
+				$this.data("map-width", 0);
+			}
+
+			$this.height($this.width());
+
+			if (($this.width() - $this.data("map-width")) > ($this.data("map-width") / 4)) {
+				$this.data("map-width", $this.width());
+				console.log("getting new map", $this.data("map-width"));
+
+				mapImgUrl = "http://maps.google.com/maps/api/staticmap?" +
+					"sensor=false&" + "scale=2&" +
+					"size=" + $this.width() + "x" + $this.height() + "&" +
+					"zoom=" + $this.data("zoom") + "&" +
+					"markers=" + addressEncoded + "&" + "center=" + addressEncoded;
+
+				$this.css({
+					"color": "black",
+					"text-shadow": "0 0 2px white",
+					"background-repeat"  : "no-repeat",
+					"background-position": "50% 50%",
+					"background-size"    : "100% 100%",
+					"background-image"   : "url('" + mapImgUrl + "')"
+				});
 			}
 		});
-	}());
+	};
 
-	// (function appendToggle() {
-	// 	$("#services .details").each(function (){
-	// 		var $this = $(this);
 
-	// 		$("<div class='text-center small' data-toggle='collapse' data-target='#" + $this.attr("id") + "'>" +
-	// 			getChevronHtml("down") +
-	// 		"</div >").insertAfter($this);
-	// 	});
-	// }());
-
-	(function buttonToggle() {
-		var getCallback = function (html) {
-			return function () {
-				$($(this).parent().find("[data-toggle=collapse]")[0]).html(html);
-			};
+	$(document).ready(function () {
+		var getChevronHtml = function (direction) {
+			return "<i class='glyphicon glyphicon-chevron-" + direction + "'></i>";
 		};
 
-		$(".details")
-			.on("show.bs.collapse", getCallback(getChevronHtml("up")))
-			.on("hide.bs.collapse", getCallback(getChevronHtml("down")));
-	}());
-});
+		(function mobileMenuRetract() {
+			var $menu   = $("#navbar-collapse");
+			var $toggle = $("#header .navbar-toggle");
+
+			$menu.on("click", "a", function () {
+				if ($toggle.is(":visible") && $menu.is(":visible")) {
+					$menu.collapse("hide");
+				}
+			});
+		}());
+
+		drawMap();
+
+		// (function appendToggle() {
+		// 	$("#services .details").each(function (){
+		// 		var $this = $(this);
+
+		// 		$("<div class='text-center small' data-toggle='collapse' data-target='#" + $this.attr("id") + "'>" +
+		// 			getChevronHtml("down") +
+		// 		"</div >").insertAfter($this);
+		// 	});
+		// }());
+
+		(function buttonToggle() {
+			var getCallback = function (html) {
+				return function () {
+					$($(this).parent().find("[data-toggle=collapse]")[0]).html(html);
+				};
+			};
+
+			$(".details")
+				.on("show.bs.collapse", getCallback(getChevronHtml("up")))
+				.on("hide.bs.collapse", getCallback(getChevronHtml("down")));
+		}());
+	});
+
+	$(window).resize(function () {
+		drawMap();
+	});
+
+}());
+
+
+
+
