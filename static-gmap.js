a003aa426b6447c8e5ce4c5795f43fa853144492
Oleg Gunkin <oleg.gunkin@gmail.com>
2013-12-05 21:21:28 -0800

    made OOP plugin

--- static-gmap.js
+++ static-gmap.js
@@ -4,7 +4,7 @@
 			var $el = $(element);
 
 			this.$element       = $el;
-			this.addressEncoded = encodeURI($el.text());
+			this.addressEncoded = encodeURI($.trim($el.text()));
 			this.mapWidth       = 0;
 			this.mapHeight      = 0;
 
@@ -20,9 +20,7 @@
 		};
 
 		var getMapURL = function (addressEncoded, size, zoom, scale) {
-			if (!scale) {
-				scale = 1;
-			}
+			scale = scale || 1;
 
 			return "http://maps.google.com/maps/api/staticmap?" +
 				"sensor=false&" + "scale=" + scale + "&" + "zoom=" + zoom + "&" +
@@ -53,27 +51,39 @@
 				var scale = getScalePercentage({"width":this.mapWidth, "height":this.mapHeight}, elSize);
 
 				var boost = function (k, n) {
-					return Math.round(k * n);
+					var result = Math.floor(k * n);
+
+					return (result < 640) ? result : 640 ;
 				};
 
-				console.log(scale);
+				if (this.mapWidth < 640 && this.mapHeight < 640) {
+					if (scale.width < 100 || scale.height < 100) {
+						mapSize = {"width": boost(1.5, elSize.width), "height": boost(1.5, elSize.height)};
 
-				if (scale.width < 100 || scale.height < 100) {
-					mapSize = {"width": boost(1.5, elSize.width), "height": boost(1.5, elSize.height)};
-					mapURL = getMapURL(this.addressEncoded, mapSize, 10, 2);
+						console.log("getting new map", mapSize.width, mapSize.height);
+						mapURL = getMapURL(this.addressEncoded, mapSize, 10, 2);
 
-					this.mapWidth  = mapSize.width;
-					this.mapHeight = mapSize.height;
+						this.mapWidth  = mapSize.width;
+						this.mapHeight = mapSize.height;
 
-					scale = getScalePercentage({"width":this.mapWidth, "height":this.mapHeight}, elSize);
-
-					this.$element.css({
-						"background-image": "url(" + mapURL + ")",
-						"background-size": scale.width + "% " + scale.height + "% "
-					});
-
+						this.$element.css({"background-image": "url(" + mapURL + ")"});
+					}
 				}
 
+				scale = getScalePercentage({"width":this.mapWidth, "height":this.mapHeight}, elSize);
+				console.log("scaling to", scale.width, scale.height);
+
+				if (scale.width < 100) {
+					scale.height = Math.round(100 * scale.height / scale.width);
+					scale.width = 100;
+				}
+
+				if (scale.height < 100) {
+					scale.width = Math.round(100 * scale.width / scale.height);
+					scale.height = 100;
+				}
+
+				this.$element.css({"background-size": scale.width + "% " + scale.height + "% "});
 			}
 		};
 
@@ -83,6 +93,12 @@
 	$(document).ready(function () {
 		$(".static-gmap").each(function () {
 			$(this).data("static-gmap", (new GMap(this)));
+		});
+	});
+
+	$(window).resize(function () {
+		$(".static-gmap").each(function () {
+			$(this).data("static-gmap").draw();
 		});
 	});
 }(jQuery));
