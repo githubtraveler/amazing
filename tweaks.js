4caa7b72769e358779bcbcfb621750cb374c86c0
Oleg Gunkin <oleg.gunkin@gmail.com>
2013-12-06 18:59:42 -0800

    cursor pointer

--- tweaks.js
+++ tweaks.js
@@ -3,23 +3,25 @@
 		$("#contact-info .static-gmap").css({"height": $("#contact-info .well").outerHeight()});
 	};
 
+	var imagesMustFit = function () {
+		$("#portfolio-carousel .item img").css({"max-height": ($(window).height() - 100) + "px"});
+	};
+
+	var mobileMenuRetract = function () {
+		var $menu   = $("#navbar-collapse");
+		var $toggle = $("#header .navbar-toggle");
+
+		$menu.on("click", "a", function () {
+			if ($toggle.is(":visible") && $menu.is(":visible")) {
+				$menu.collapse("hide");
+			}
+		});
+	};
+
 	$(document).ready(function () {
-		var getChevronHtml = function (direction) {
-			return "<i class='glyphicon glyphicon-chevron-" + direction + "'></i>";
-		};
-
-		linkHeights();
-
-		(function mobileMenuRetract() {
-			var $menu   = $("#navbar-collapse");
-			var $toggle = $("#header .navbar-toggle");
-
-			$menu.on("click", "a", function () {
-				if ($toggle.is(":visible") && $menu.is(":visible")) {
-					$menu.collapse("hide");
-				}
-			});
-		}());
+		// var getChevronHtml = function (direction) {
+		// 	return "<i class='glyphicon glyphicon-chevron-" + direction + "'></i>";
+		// };
 
 		// (function appendToggle() {
 		// 	$("#services .details").each(function (){
@@ -31,21 +33,26 @@
 		// 	});
 		// }());
 
-		(function buttonToggle() {
-			var getCallback = function (html) {
-				return function () {
-					$($(this).parent().find("[data-toggle=collapse]")[0]).html(html);
-				};
-			};
+		// (function buttonToggle() {
+		// 	var getCallback = function (html) {
+		// 		return function () {
+		// 			$($(this).parent().find("[data-toggle=collapse]")[0]).html(html);
+		// 		};
+		// 	};
 
-			$(".details")
-				.on("show.bs.collapse", getCallback(getChevronHtml("up")))
-				.on("hide.bs.collapse", getCallback(getChevronHtml("down")));
-		}());
+		// 	$(".details")
+		// 		.on("show.bs.collapse", getCallback(getChevronHtml("up")))
+		// 		.on("hide.bs.collapse", getCallback(getChevronHtml("down")));
+		// }());
+
+		mobileMenuRetract();
+		linkHeights();
+		imagesMustFit();
 	});
 
 	$(window).resize(function (){
 		linkHeights();
+		imagesMustFit();
 	});
 }());
 
