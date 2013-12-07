d93355f1e2b819e874c289ade324a3ceb9892ac1
Oleg Gunkin <oleg.gunkin@gmail.com>
2013-12-07 11:29:10 -0800

    cleaned up commented code

--- src/js/tweaks.js
+++ src/js/tweaks.js
@@ -25,32 +25,6 @@
 	};
 
 	$(document).ready(function () {
-		// var getChevronHtml = function (direction) {
-		// 	return "<i class='glyphicon glyphicon-chevron-" + direction + "'></i>";
-		// };
-
-		// (function appendToggle() {
-		// 	$("#services .details").each(function (){
-		// 		var $this = $(this);
-
-		// 		$("<div class='text-center small' data-toggle='collapse' data-target='#" + $this.attr("id") + "'>" +
-		// 			getChevronHtml("down") +
-		// 		"</div >").insertAfter($this);
-		// 	});
-		// }());
-
-		// (function buttonToggle() {
-		// 	var getCallback = function (html) {
-		// 		return function () {
-		// 			$($(this).parent().find("[data-toggle=collapse]")[0]).html(html);
-		// 		};
-		// 	};
-
-		// 	$(".details")
-		// 		.on("show.bs.collapse", getCallback(getChevronHtml("up")))
-		// 		.on("hide.bs.collapse", getCallback(getChevronHtml("down")));
-		// }());
-
 		mobileMenuRetract();
 		linkHeights();
 		imagesMustFit();
