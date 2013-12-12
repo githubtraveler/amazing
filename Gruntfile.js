5cc0dccc54fa889f30f374e317de8934a129e037
Oleg Gunkin <oleg.gunkin@gmail.com>
2013-12-12 12:29:10 -0800

    fixed missing js files in lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd   : "src",
-					src   : "*.js",
+					src   : "***/*.js",
 					ext   : ".js",
 					dest  : "dist"
 				}]
